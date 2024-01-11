b435015822be777e572bf5ba2e6e4b343eceb6a8
yagofeitoza19 <113868936+yagofeitoza19@users.noreply.github.com>
2024-01-11 10:46:31 -0400

    Create main.c

---
+++ 20240109/20240109-01/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "paciente.h"
+
+int main() {
+    TabelaHash *tabela = criarTabelaHash();
+
+    int codigo;
+    char nome[50];
+    char cpf[15];
+    int idade;
+
+    for (int i = 0; i < 3; i++) {
+        printf("Insira os dados do paciente %d:\n", i + 1);
+
+        printf("Código: ");
+        scanf("%d", &codigo);
+
+        printf("Nome: ");
+        scanf("%s", nome);
+
+        printf("CPF: ");
+        scanf("%s", cpf);
+
+        printf("Idade: ");
+        scanf("%d", &idade);
+
+        inserirPaciente(tabela, codigo, nome, cpf, idade);
+    }
+
+    int codigoBusca;
+    printf("Digite o código do paciente para busca: ");
+    scanf("%d", &codigoBusca);
+
+    NodoPaciente *p = buscarPaciente(tabela, codigoBusca);
+    if (p != NULL) {
+        printf("Paciente encontrado: Código %d, Nome: %s, CPF: %s, Idade: %d\n", p->codigo, p->nome, p->cpf, p->idade);
+    } else {
+        printf("Paciente não encontrado.\n");
+    }
+
+    imprimirTabela(tabela);
+
+    liberarTabelaHash(tabela);
+
+    return 0;
+}
