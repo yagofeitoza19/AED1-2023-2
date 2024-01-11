f265ef4e41aaf93eb1ed236cd3664bf50d4b3115
yagofeitoza19 <113868936+yagofeitoza19@users.noreply.github.com>
2024-01-11 11:20:10 -0400

    Create paciente.h

---
+++ 20240111/20240111 Tabela Hash 2 - 01/paciente.h
@@ -0,0 +1,28 @@
+#ifndef PACIENTE_H
+#define PACIENTE_H
+
+typedef struct {
+    int codigo;
+    char nome[50];
+    char cpf[15];
+    int idade;
+    int disponivel;
+} Paciente;
+
+typedef struct NodoPaciente {
+    Paciente paciente;
+    struct NodoPaciente *prox;
+} NodoPaciente;
+
+typedef struct {
+    NodoPaciente *pacientes[100];
+} TabelaHash;
+
+TabelaHash *criarTabelaHash();
+void inserirPaciente(TabelaHash *tabela, int codigo, const char *nome, const char *cpf, int idade);
+Paciente *buscarPaciente(TabelaHash *tabela, int codigo);
+void excluirPaciente(TabelaHash *tabela, int codigo);
+void imprimirTabela(TabelaHash *tabela);
+void liberarTabelaHash(TabelaHash *tabela);
+
+#endif
